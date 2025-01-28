6c793eed21198b1e63feb09fb1512c202f8ca977
Kjell Inge Tomren <kitomren@gmail.com>
2025-01-28 16:29:57 +0100

    hello world with firstname and lastname

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
-export const greeting = (name: string) => {
-	const.log(`Hello ${name}`);
+/**
+ * Function that greets a person
+ * @param firstname First name
+ * @param lastname Last name
+ */
+
+
+export const greeting = (firstname: string, lastname: string) => {
+	const.log(`Hello ${firstname} ${lastname}`);
 };
