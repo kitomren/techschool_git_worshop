d5acfb65bd338afc3797985257e701c2a1257405
Kjell Inge Tomren <kitomren@gmail.com>
2025-01-28 18:29:51 +0100

    greeting with 4 people in gang

--- index.ts
+++ index.ts
@@ -8,3 +8,18 @@
 export const greeting = (firstname: string, lastname: string) => {
 	const.log(`Hello ${firstname} ${lastname}`);
 };
+
+const people = [
+	{first name: "Arne", lastname: "Arnesen" },
+	{first name: "Magnus", lastname: "Magnusson" },
+	{first name: "Siri", lastname: "Sirisen" },
+	{first name: "Stefan", lastname: "Stefanson" },
+
+];
+
+export const greetTheGang = () => {
+	people.forEach((person) => {
+		greeting(person.firstname, person.lastname);
+	});
+};
+
